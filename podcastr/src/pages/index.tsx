3c3ae0e566c48032f08ce8e998fe28d6f8ed3a44
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-04-20 11:20:11 -0300

    using SSG to render data from a fake api

---
+++ podcastr/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home(props) {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
