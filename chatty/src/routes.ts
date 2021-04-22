752afef9b3ea0825ca4dc214b9e290494a24fb66
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-04-22 13:27:22 -0300

    creting messages and User

--- chatty/src/routes.ts
+++ chatty/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
+import { MessagesController } from './controllers/MessagesController';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
-
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 /*
   Routes Params => parametros de rotas  http://localhost:3333/settings/1
   Query Params => Filtros e buscas  http://localhost:3333/settings/1?search=algumacoisa
@@ -13,5 +16,10 @@ const settingsController = new SettingsController();
 */
 
 routes.post('/settings', settingsController.create);
+
+routes.post('/users', usersController.create);
+
+routes.post('/messages', messagesController.create);
+routes.get('/messages/:id', messagesController.showByUser);
 
 export { routes };
