564af2e7909029f77a1c47e46688bb9a86d82ba4
Gabriel-J3sus <gabr.jesus001@gmail.com>
2021-04-24 12:36:29 -0300

    end of project

--- chatty/src/services/ConnectionsService.ts
+++ chatty/src/services/ConnectionsService.ts
@@ -35,6 +35,29 @@ class ConnectionsService {
 
     return connection;
   }
+
+  async findAllWithoutAdmin() {
+    const connections = await this.connectionRespository.find({
+      where: { admin_id: null },
+      relations: ["user"],
+    })
+
+    return connections
+  }
+
+  async findBySocketID(socket_id: string) {
+    const connection = await this.connectionRespository.findOne({
+      socket_id
+    })
+
+    return connection;
+  }
+
+  async updateAdminID(user_id: string, admin_id: string) {
+    await this.connectionRespository.createQueryBuilder().update(Connection).set({ admin_id }).where("user_id = :user_id", {
+      user_id
+    }).execute();
+  }
 }
 
 export { ConnectionsService }
